8c692401e77f0ab8592adcb7f1d9a1b1dae9984d
Sam Ireland <sam.ireland.uk@gmail.com>
2016-08-13 23:22:22 +0100

    Home page URL

--- budgeter/urls.py
+++ budgeter/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from home import urls as home_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(home_urls)),
 ]
