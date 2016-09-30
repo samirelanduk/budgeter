a000afce5eb24dbc2ff1e1b9e7ba298b27987d09
Sam Ireland <sam.ireland.uk@gmail.com>
2016-09-30 21:27:58 +0100

    Start accounts app

--- budgeter/settings.py
+++ budgeter/settings.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'accounts'
 )
 
 MIDDLEWARE_CLASSES = (
