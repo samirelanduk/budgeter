75f9789fb7c593939f29b609b2e2e28403c51d3b
samirelanduk <sam.ireland.uk@gmail.com>
2016-10-28 00:12:45 +0100

    Add incorrect login FT

--- ftests/test_auth.py
+++ ftests/test_auth.py
@@ -102,3 +102,49 @@ class AccountLoginTests(FunctionalTest):
          self.browser.find_element_by_tag_name("h2").text,
          "Persona Una"
         )
+
+
+    def test_invalid_credentials_wont_login(self):
+        # User goes to login page
+        self.browser.get(self.live_server_url + "/users/login/")
+
+        # They try to login in with an incorrect email
+        form = self.browser.find_element_by_tag_name("form")
+        inputs = form.find_elements_by_tag_name("input")
+        inputs[0].send_keys("wrong@s.com")
+        inputs[1].send_keys("secret1")
+        inputs[-1].click()
+
+        # They are stll on the login page
+        self.assertEqual(
+         self.browser.current_url,
+         self.live_server_url + "/users/login/"
+        )
+
+        # There is an error message
+        errors = self.browser.find_element_by_id("login_errors")
+        self.assertEqual(
+         errors.text,
+         "Credentials incorrect."
+        )
+
+        # They try again with an incorrect password
+        self.browser.get(self.live_server_url + "/users/login/")
+        form = self.browser.find_element_by_tag_name("form")
+        inputs = form.find_elements_by_tag_name("input")
+        inputs[0].send_keys("p1@s.com")
+        inputs[1].send_keys("wrongpassword")
+        inputs[-1].click()
+
+        # They are stll on the login page
+        self.assertEqual(
+         self.browser.current_url,
+         self.live_server_url + "/users/login/"
+        )
+
+        # There is an error message
+        errors = self.browser.find_element_by_id("login_errors")
+        self.assertEqual(
+         errors.text,
+         "Credentials incorrect."
+        )
